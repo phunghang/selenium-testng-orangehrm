57937a765e222839a4ebda6ea4099f42f4a3050a
Hang <phungthihang@gobiz.vn>
2024-06-02 18:41:41 +0700

    commit thử

--- interfaces/pageUIs/LoginUI.java
+++ interfaces/pageUIs/LoginUI.java
@@ -4,4 +4,5 @@ public class LoginUI {
 	public static final String USERNAME_TEXTBOX = "name=username";
 	public static final String PASSWORD_TEXTBOX = "name=password";
 	public static final String LOGIN_BT = "css=.orangehrm-login-button";
+	
 }
